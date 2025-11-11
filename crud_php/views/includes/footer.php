6278805dd7cc206fc69ff331b13ccbe8744fe090
PamonhaMaker <gabrizdiniz@gmail.com>
2025-11-11 18:59:33 -0300

    Add files via upload

---
+++ crud_php/views/includes/footer.php
@@ -0,0 +1,7 @@
+        <hr>
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?>Grabriel Diniz. Todos os direitos são meus</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ crud_php/views/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title>O Melhor Cardapio Do Mundo</title>
+    <link rel="stylesheet" href="/crud_php/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>CARDAPIO DO DON DINIZ</h1>
+        <nav>
+            <a href="/crud_php/public/index.php?page=produtos">Cardapio</a>
+            <a href="/crud_php/public/index.php?page=categorias">Tipos de receita</a>
+        </nav>
+        <hr>
